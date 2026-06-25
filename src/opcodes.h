8f8d993d04e58fd8969471644676a16a8ba37a71
Maggie Michelsen <maggiemichelsen42@gmail.com>
2026-06-25 15:29:37 -0600

    src folder

---
+++ src/opcodes.h
@@ -0,0 +1,13 @@
+#IFNDEF OPCODES.H
+#DEFINE OPCODES.H
+
+// Arithmetic Ops
+int ADD(int src1, int src2);
+int SUB(int src1, int src2);
+int MUL(int src1, int src2);
+int DIV(int src1, int src2);
+int MOD(int src1, int src2);
+
+
+
+#ENDIF
