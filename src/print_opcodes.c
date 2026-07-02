a8f6aa93b4678bc8a92919c5dca613664b797d5a
Maggie Michelsen <maggiemichelsen42@gmail.com>
2026-07-02 16:59:55 -0600

    opcodes

--- src/print_opcodes.c
+++ src/print_opcodes.c
@@ -6,11 +6,76 @@ int main(void) {
 	int DIV = 'D' + 'I' + 'V';
 	int MUL = 'M' + 'U' + 'L';
 	int MOD = 'M' + 'O' + 'D';
-	ADD = ADD >> 4;
+
+	int JMP = 'J' + 'M' + 'P';
+	int JNE = 'J' + 'N' + 'E';
+	int JEQ = 'J' + 'E' + 'Q';
+	int JGR = 'J' + 'G' + 'R';
+	int JLE = 'J' + 'L' + 'E';
+	int CMP = 'C' + 'M' + 'P';
+	int TST = 'T' + 'S' + 'T';
+	int HLT = 'H' + 'L' + 'T';
+	int NOP = 'N' + 'O' + 'P';
+	int CLL = 'C' + 'L' + 'L';
+	int RET = 'R' + 'E' + 'T';
+	int TRP = 'T' + 'R' + 'P';
+	int KLL = 'K' + 'L' + 'L';
+	int HCF = 'H' + 'C' + 'F';
+
+	int AND = 'A' + 'N' + 'D';
+	int OR = ' ' + 'O' + 'R';
+	int XOR = 'X' + 'O' + 'R';
+	int NOR = 'N' + 'O' + 'R';
+	int NOT = 'N' + 'O' + 'T';
+	int LSR = 'L' + 'S' + 'R';
+	int LSL = 'L' + 'S' + 'L';
+
+	int MOV = 'M' + 'O' + 'V';
+	int SWP = 'S' + 'W' + 'P';
+	int PSH = 'P' + 'S' + 'H';
+	int POP = 'P' + 'O' + 'P';
+	int LEA = 'L' + 'E' + 'A';
+	int IN = ' ' + 'I' + 'N';
+	int OUT = 'O' + 'U' + 'T';
+	int END = 'E' + 'N' + 'D';
+
 	printf("ADD: %d\n", ADD);
 	printf("SUB: %d\n", SUB);
 	printf("DIV: %d\n", DIV);
 	printf("MUL: %d\n", MUL);
 	printf("MOD: %d\n", MOD);
+
+	printf("JMP: %d\n", JMP);
+	printf("JNE: %d\n", JNE);
+	printf("JEQ: %d\n", JEQ);
+	printf("JGR: %d\n", JGR);
+	printf("JLE: %d\n", JLE);
+	printf("CMP: %d\n", CMP);
+	printf("TST: %d\n", TST);
+	printf("HLT: %d\n", HLT);
+	printf("NOP: %d\n", NOP);
+	printf("CLL: %d\n", CLL);
+	printf("RET: %d\n", RET);
+	printf("TRP: %d\n", TRP);
+	printf("KLL: %d\n", KLL);
+	printf("HCF: %d\n", HLT);
+
+	printf("AND: %d\n", AND);
+	printf("OR: %d\n", OR);
+	printf("XOR: %d\n", XOR);
+	printf("NOR: %d\n", NOR);
+	printf("NOT: %d\n", NOT);
+	printf("LSR: %d\n", LSR);
+	printf("LSL: %d\n", LSL);
+
+	printf("MOV: %d\n", MOV);
+	printf("SWP: %d\n", SWP);
+	printf("PSH: %d\n", PSH);
+	printf("POP: %d\n", POP);
+	printf("LEA: %d\n", LEA);
+	printf("IN: %d\n", IN);
+	printf("OUT: %d\n", OUT);
+	printf("END: %d\n", END);
+
 	return 0;
 }
