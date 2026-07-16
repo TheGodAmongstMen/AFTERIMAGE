ce5beb97de2b4c68d82457cd78d78461525de304
Maggie Michelsen <maggiemichelsen42@gmail.com>
2026-07-16 16:56:14 -0600

    refactored code (switched to C)

--- src/structs.h
+++ src/structs.h
@@ -3,7 +3,21 @@
 
 #define OPCODE(a, b, c) (((a - 1) << 6) | ((b - 1) << 2) | ((c)))
 
+#include <stdint.h>
 // register struct
+struct regs {
+	uint16_t ACC;
+	uint32_t r1;
+	uint32_t r2;
+	uint32_t r3;
+	uint32_t TEST;
+	uint8_t FLE;
+	uint32_t PC;
+	uint32_t SP;
+	uint32_t LR;
+	uint32_t SIG;
+	uint32_t cycles;
+};
 
 // struct for user written code
 struct program_code {
@@ -20,5 +34,6 @@ struct shell_code {
 	int src2;
 	int dest;
 };
+
 
 #endif
